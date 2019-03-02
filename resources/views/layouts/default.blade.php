21cdbf41e058e0ecc01f0fe6f7f7cab826cc8c38
Francis Sowani <frathoso@gmail.com>
2019-03-02 22:08:27 +0400

    Add Google Analytics

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,22 +38,20 @@
     <meta name="description" content="" >
     <meta name="author" content="" >
     <meta name="keywords" content="STEM Loyola, STEM, Loyola, High School, Hacks, Tanzania, programming, learning, coding, hacking, tutorials">
-    
     {{-- Social Media Tags http://moz.com/blog/meta-data-templates-123 --}}
-    {{--} Twitter Card data --}}
+        {{--} Twitter Card data --}}
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:site" content="@STEMLoyola">
     <meta name="twitter:title" content="STEM Loyola | {!! $d_page['title'] !!}" />
     <meta name="twitter:description" content="STEMLoyola Challenges" />
     <meta name="twitter:creator" content="@STEMLoyola" />
-    {{-- Open Graph data --}}
+        {{-- Open Graph data --}}
     <meta property="og:title" content="STEMLoyola | {!! $d_page['title'] !!}" />
     <meta property="og:type" content="article" />
     <meta property="og:url" content="{!! route('home.public') !!}" />
     <meta property="og:image" content="/assets/images/stem-loyola-con.png" />
     <meta property="og:description" content="STEMLoyola Challenges" />
     <meta property="og:site_name" content="STEMLoyola Challenges" />
-    {{--  Social Media Tags end --}}
     
     {{-- Website icons --}}
     <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
@@ -75,7 +73,7 @@
     <meta name="theme-color" content="#ffffff">
     
     {{--  Vendor and custom styles  --}}
-    <link href="/assets/css/app.css?v=0.0.1" rel="stylesheet" type="text/css" >
+    <link href="/assets/css/app.css?v=0.1" rel="stylesheet" type="text/css" >
   </head>
   <body>
     <div class="container">
@@ -84,9 +82,16 @@
       @yield('footer')
 
       {{--  Vendor and custom scripts  --}}
-      <script src="/assets/js/app.js?v=0.0.1"></script>
+      <script src="/assets/js/app.js?v=0.1"></script>
+
       {{-- Set up Google Analytics --}}
-      {{-- TODO --}}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135436218-1"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-135436218-1');
+      </script>
     </div>
   </body>
 </html>
