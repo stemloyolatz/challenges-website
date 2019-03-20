1227bb7fc77aa821805173c7da6eefbc86e036a0
Francis Sowani <frathoso@gmail.com>
2019-03-20 06:26:15 -0700

    Paint icons

--- resources/views/pages/home/public.blade.php
+++ resources/views/pages/home/public.blade.php
@@ -34,11 +34,11 @@ $d_page = array(
   <div class="sl-ospace-3 "></div>
   <div class="row">
     <div class="col-sm sl-center-t sl-ospace-1 sl-challenge-left">
-        <h3><i class="fas fa-caret-right"></i> For the Novice</h3>
+        <h3><i class="fas fa-caret-right sl-paint"></i> For the Novice</h3>
         <h4 class="sl-ospace-1"><b>Greetings</b></h4>
         <h1 class="sl-ospace-1">@emojione(':hugging:')</h1>
-        <p class="summary"><i>Create your very first program and use it to tell us few things about yourself!</i></p>
-        <h5><i class="fas fa-lg fa-file-code"></i> Download Resources for:</h5>
+        <p class="sl-summary"><i>Create your very first program and use it to tell us few things about yourself!</i></p>
+        <h5 class="sl-ospace-1"><i class="fas fa-lg fa-file-code sl-paint"></i> Download Resources for:</h5>
         <div class="sl-ospace-1 "></div>
         <table class="table table-bordered table-hover">
           <thead>
@@ -63,11 +63,11 @@ $d_page = array(
         </table>
     </div>
     <div class="col-sm sl-center-t sl-ospace-1 sl-challenge-right">
-        <h3><i class="fas fa-caret-right"></i> For the Experienced</h3>
+        <h3><i class="fas fa-caret-right sl-paint"></i> For the Experienced</h3>
         <h4 class="sl-ospace-1"><b>Jane in Trouble</b></h4>
         <h1 class="sl-ospace-1">@emojione(':cry:')</h1>
-        <p class="summary"><i>Jane has written a program to help her complete her Maths homework quickly. Unfortunately, she is facing some few issues with the program. Can you help her fix it?</i></p>
-        <h5><i class="fas fa-lg fa-file-code"></i> Download Resources for:</h5>
+        <p class="sl-summary"><i>Jane has written a program to help her complete her Maths homework quickly. Unfortunately, she is facing some few issues with the program. Can you help her fix it?</i></p>
+        <h5 class="sl-ospace-1"><i class="fas fa-lg fa-file-code sl-paint"></i> Download Resources for:</h5>
         <div class="sl-ospace-1 "></div>
         <table class="table table-bordered table-hover">
           <thead>
