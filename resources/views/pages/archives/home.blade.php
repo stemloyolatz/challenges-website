8a03eee80ca37d90254769751ac2275c38366ce0
Francis Sowani <frathoso@gmail.com>
2019-08-28 22:34:33 +0400

    Fix past challenges

--- resources/views/pages/archives/home.blade.php
+++ resources/views/pages/archives/home.blade.php
@@ -1,7 +1,7 @@
 {{-- Declare required page constants --}}
 <?php
 $d_page = array(
-    'title' => 'Past Challenge',
+    'title' => 'Archives',
     'description' => 'This is the past challenges archives page!'
 );
 ?>
@@ -23,7 +23,107 @@ $d_page = array(
   --}}
 @section('body')
 <div class="container sl-cont">
-  Archives
+  <div class="row  sl-cont">
+      <h2 class="sl-ospace-1 sl-center-c"><strong>{{ $d_page['title'] }}</strong></h2>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col col-sm-12 col-md-8 col-lg-6 sl-center-t">
+        <p><i>Last updated: 28 August 2019</i></p>
+    </div>
+  </div>
+  <div class="sl-ospace-2"></div>
+  <div class="row  sl-cont">
+    <h4 class="sl-ospace-1 sl-center-c"><strong>Challenges</strong></h4>
+  </div>
+  <div class="row justify-content-center">
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
+          <tr class="sl-center-t">
+            <th class="align-middle" scope="col">ID</th>
+            <th class="align-middle" scope="col">Level</th>
+            <th class="align-middle" scope="col">Title</th>
+            <th class="align-middle" scope="col">Solutions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {{-- Challenge 01 --}}
+          <tr>
+            <th rowspan="2" class="align-middle" scope="row">01</th>
+            <td class="align-middle">Novice</td>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-03-A') }}">Greetings</a></td>
+            <td class="align-middle">WIP</td>
+          </tr>
+          <tr>
+            <th class="align-middle">Experienced</th>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-03-A') }}">Jane in Trouble</a></td>
+            <td class="align-middle"><a href="{{ url('/assets/challenges/19.03/19.03-A2-solution-cpp.zip') }}">C++</a></td>
+          </tr>
+          {{-- Challenge 02 --}}
+          <tr>
+            <th rowspan="2" class="align-middle" scope="row">02</th>
+            <td class="align-middle">Novice</td>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-04-A') }}">Mt. Kilimanjaro</a></td>
+            <td class="align-middle">WIP</td>
+          </tr>
+          <tr>
+            <th class="align-middle">Experienced</th>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-04-A') }}">Form Four Results</a></td>
+            <td class="align-middle"><a href="{{ url('/assets/challenges/19.04/19.04-A2-solution-cpp.zip') }}">C++</a></td>
+          </tr>
+          {{-- Challenge 03 --}}
+          <tr>
+            <th rowspan="2" class="align-middle" scope="row">03</th>
+            <td class="align-middle">Novice</td>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-05-A') }}">Bouncing Ball (Part 1)</a></td>
+            <td class="align-middle">WIP</td>
+          </tr>
+          <tr>
+            <th class="align-middle">Experienced</th>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-05-A') }}">Forbidden List of Primes</a></td>
+            <td class="align-middle"><a href="{{ url('/assets/challenges/19.05/19.05-A2-solution-cpp.zip') }}">C++</a></td>
+          </tr>
+          {{-- Challenge 04 --}}
+          <tr>
+            <th rowspan="2" class="align-middle" scope="row">04</th>
+            <td class="align-middle">Novice</td>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-08-A') }}">Gender Code</a></td>
+            <td class="align-middle">WIP</td>
+          </tr>
+          <tr>
+            <th class="align-middle">Experienced</th>
+            <td class="align-middle"><a href="{{ url('/archives/challenges/19-08-A') }}">Famous Ward Names</a></td>
+            <td class="align-middle"><a href="{{ url('/assets/challenges/19.08/19.08-A2-solution-cpp.zip') }}">C++</a></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+  <div class="sl-ospace-2"></div>
+  <div class="row  sl-cont">
+    <h4 class="sl-ospace-1 sl-center-c"><strong>Projects</strong></h4>
+  </div>
+  <div class="row justify-content-center">
+    <table class="table table-bordered table-striped table-hover">
+        <thead>
+          <tr class="sl-center-t">
+            <th class="align-middle" scope="col">ID</th>
+            <th class="align-middle" scope="col">Level</th>
+            <th class="align-middle" scope="col">Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {{-- Project 01 --}}
+          <tr>
+            <th rowspan="2" class="align-middle" scope="row">01</th>
+            <td class="align-middle">Novice</td>
+            <td class="align-middle"><a href="{{ url('/archives#') }}">WIP</a></td>
+          </tr>
+          <tr>
+            <th class="align-middle">Experienced</th>
+            <td class="align-middle"><a href="{{ url('/archives#') }}">Building a Virus Cleanup Tool</a></td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 </div>
 @stop
 
